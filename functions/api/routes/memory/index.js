e035b350b7c25a922443d2c6acea5ebc301068f9
Crayon <lsh328328@naver.com>
2021-11-21 04:35:14 +0900

    [fix]: url duplication fix

--- functions/api/routes/memory/index.js
+++ functions/api/routes/memory/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/:month", require("./memoryListGET"));
+router.get("/list/:month", require("./memoryListGET"));
 router.get("/:memoryId", require("./memoryGET"));
 
 module.exports = router;
