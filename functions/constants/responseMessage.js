2a50acdebc0209a641eec809c7b96211a3bb0332
yoon <nicealice99@gmail.com>
2021-11-21 03:30:10 +0900

    [feat]: add responseMessage

--- functions/constants/responseMessage.js
+++ functions/constants/responseMessage.js
@@ -1,18 +1,16 @@
 module.exports = {
-    NULL_VALUE: "필요한 값이 없습니다",
-    OUT_OF_VALUE: "파라미터 값이 잘못되었습니다",
+  NULL_VALUE: "필요한 값이 없습니다",
+  OUT_OF_VALUE: "파라미터 값이 잘못되었습니다",
 
-    // 회원가입
-    CREATED_USER: "회원 가입 성공",
-    DELETE_USER: "회원 탈퇴 성공",
-    ALREADY_EMAIL: "이미 사용중인 이메일입니다.",
+  // memory
+  MEMORY_GET_SUCCESS: "게시글 조회 성공",
+  MEMORY_LIST_GET_SUCCESS: "게시글 목록 조회",
+  MEMORY_POST_SUCCESS: "게시글 작성 성공",
+  NO_MEMORY: "존재하지 않는 게시글입니다",
 
-    // 로그인
-    LOGIN_SUCCESS: "로그인 성공",
-    LOGIN_FAIL: "로그인 실패",
-    NO_USER: "존재하지 않는 회원입니다.",
-    MISS_MATCH_PW: "비밀번호가 맞지 않습니다.",
+  // profile
+  PROFILE_GET_SUCCESS: "유저 프로필 조회 성공",
 
-    // 프로필 조회
-    READ_PROFILE_SUCCESS: "프로필 조회 성공",
+  // 서버 내 오류
+  INTERNAL_SERVER_ERROR: "서버 내 오류"
 };
