bac3249a9461b9c5ac9bcd8829db91e3f3839dcb
Crayon <lsh328328@naver.com>
2021-11-21 02:11:53 +0900

    swagger settings

--- functions/api/routes/post/index.js
+++ functions/api/routes/post/index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
+/**
+ * @swagger
+ * /post/regist:
+ */
 router.post("/regist", require("./postingPOST"));
 
 module.exports = router;
--- functions/modules/swagger.js
+++ functions/modules/swagger.js
@@ -1,22 +1,34 @@
-const swaggerUi = require('swagger-ui-express');
-const swaggereJsdoc = require('swagger-jsdoc');
+const swaggerUi = require("swagger-ui-express");
+const swaggereJsdoc = require("swagger-jsdoc");
 
 const options = {
-    swaggerDefinition: {
-        info: {
-            title: 'Familing API',
-            version: '1.0.0',
-            description: 'Familing API with express',
-        },
-        host: 'localhost:3300',
-        basePath: '/'
+  swaggerDefinition: {
+    info: {
+      title: "Familing API",
+      version: "1.0.0",
+      description: "Familing API with express"
     },
-    apis: ['../api/routes/*.js', './swagger/*']
+    host: "localhost:3300",
+    basePath: "/",
+    schemes: ["http", "https"],
+    definitions: {
+      Post: {
+        type: "object",
+        properties: {
+          postId: { type: "number" },
+          category: { type: "number" },
+          date: { type: "timestamp" },
+          content: { type: "string" }
+        }
+      }
+    }
+  },
+  apis: ["./api/routes/post/*.js", "./swagger/*"]
 };
 
 const specs = swaggereJsdoc(options);
 
 module.exports = {
-    swaggerUi,
-    specs
+  swaggerUi,
+  specs
 };
