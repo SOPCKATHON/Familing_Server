1e179d3590943d6920018ab178998b08d7644cd7
Crayon <lsh328328@naver.com>
2021-11-21 07:22:59 +0900

    [fix] swagger delete

--- functions/api/index.js
+++ functions/api/index.js
@@ -6,7 +6,6 @@ const cookieParser = require("cookie-parser");
 const dotenv = require("dotenv");
 const hpp = require("hpp");
 const helmet = require("helmet");
-const { swaggerUi, specs } = require('../modules/swagger');
 
 // 보안 상 깃허브에 올리면 안 되는 정보를 .env라는 파일로 관리하기 위해 사용하는 모듈
 dotenv.config();
@@ -19,8 +18,8 @@ app.use(cors());
 
 // 보안을 위한 미들웨어
 if (process.env.NODE_ENV === "production") {
-    app.use(hpp());
-    app.use(helmet());
+  app.use(hpp());
+  app.use(helmet());
 }
 
 // request에 담긴 정보를 json 형태로 파싱하기 위한 미들웨어
@@ -31,27 +30,30 @@ app.use(cookieParser());
 // 라우팅: routes 폴더로 관리
 app.use("/", require("./routes"));
 
-// swagger 라우팅
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
-
 // route 폴터에 우리가 지정할 경로가 아닌 다른 경로로 요청이 올 경우
 // 잘못된 경로로 요청이 들어왔다는 메시지를 클라이언트에 보냄
 app.use("*", (req, res) => {
-    res.status(404).json({
-        status: 404,
-        success: false,
-        message: "잘못된 경로입니다.",
-    });
+  res.status(404).json({
+    status: 404,
+    success: false,
+    message: "잘못된 경로입니다."
+  });
 });
 
 // express를 firebase functions로 감싸주는 코드
 module.exports = functions
-    .runWith({
-        timeoutSeconds: 300,
-        memory: "512MB",
-    })
-    .region("asia-northeast3")
-    .https.onRequest(async (req, res) => {
-        console.log("\n\n", "[api]", `[${req.method.toUpperCase()}]`, req.originalUrl, req.body);
-        return app(req, res);
-    })
+  .runWith({
+    timeoutSeconds: 300,
+    memory: "512MB"
+  })
+  .region("asia-northeast3")
+  .https.onRequest(async (req, res) => {
+    console.log(
+      "\n\n",
+      "[api]",
+      `[${req.method.toUpperCase()}]`,
+      req.originalUrl,
+      req.body
+    );
+    return app(req, res);
+  });
--- functions/api/routes/profile/index.js
+++ functions/api/routes/profile/index.js
@@ -1,20 +1,6 @@
 const express = require("express");
 const router = express.Router();
 
-/**
- * @swagger
- * tags:
- *  name: Profile
- *  description: Profile management
- */
-
-/**
- * @swagger
- * /profile:
- *  get:
- *    summary: 사용자 메모리 조회 API
- *    tags: [Profile]
- */
 router.get("/", require("./profileGET"));
 
 module.exports = router;
